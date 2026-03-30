a41db4c29b81e1296d507d6a362695ec01bdea4f
Daniel Hamner <Hamner88@gmail.com>
2026-03-31 00:02:06 +0200

    feat: implement GetByRegistrationAsync in VehicleRepository with in-memory mock

--- src/IntegrationLayer.VehicleService/Repositories/VehicleRepository.cs
+++ src/IntegrationLayer.VehicleService/Repositories/VehicleRepository.cs
@@ -7,6 +7,14 @@ public class VehicleRepository : IVehicleRepository
 {
     private readonly HttpClient _httpClient;
 
+    private static readonly Dictionary<string, VehicleRegistrationModel> _registrationMocks =
+        new(StringComparer.OrdinalIgnoreCase)
+        {
+            ["ABC123"] = new() { RegistrationNumber = "ABC123", Make = "Volvo", Model = "XC60", Year = 2021, Color = "Black" },
+            ["XYZ789"] = new() { RegistrationNumber = "XYZ789", Make = "Saab", Model = "9-3", Year = 2008, Color = "Silver" },
+            ["DEF456"] = new() { RegistrationNumber = "DEF456", Make = "Volvo", Model = "V70", Year = 2015, Color = "White" },
+        };
+
     public VehicleRepository(HttpClient httpClient)
     {
         _httpClient = httpClient;
@@ -22,4 +30,7 @@ public class VehicleRepository : IVehicleRepository
         return await _httpClient.GetFromJsonAsync<IEnumerable<VehicleModel>>("vehicles", cancellationToken)
                ?? Enumerable.Empty<VehicleModel>();
     }
+
+    public Task<VehicleRegistrationModel?> GetByRegistrationAsync(string registrationNumber, CancellationToken cancellationToken = default)
+        => Task.FromResult(_registrationMocks.GetValueOrDefault(registrationNumber));
 }
