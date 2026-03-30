545b4f8a4fa269b616224e8fda5fb74dfc93cb54
Daniel Hamner <Daniel.hamner@in.se>
2026-03-31 01:27:50 +0200

    InsuranceSummaryModel improvement

--- src/IntegrationLayer.Core/Models/InsuranceSummaryModel.cs
+++ src/IntegrationLayer.Core/Models/InsuranceSummaryModel.cs
@@ -1,8 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace IntegrationLayer.Core.Models;
 
 public class InsuranceSummaryModel
 {
     public InsuranceType Type { get; set; }
     public decimal MonthlyCost { get; set; }
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public VehicleRegistrationModel? Vehicle { get; set; }
 }
